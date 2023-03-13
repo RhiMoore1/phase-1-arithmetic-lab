adebfdd03e6b3a6499f9a712a3488c946d101349
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-03-13 09:28:59 -0700

    completes lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 1) + 1;
+
+let num3 = 44;
+let num4 = 10;
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 20);
